2e6d1b784f6468a7fd8d6b80647ca9c465e379ad
Sasukehteme <jackwirata@gmail.com>
2024-07-14 01:09:21 +1000

    Initial Commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,53 @@
 import Image from "next/image";
+import Link from "next/link";
+import { EmblaOptionsType } from 'embla-carousel'
+import EmblaCarousel from './ui/EmblaCarousel'
+
+const OPTIONS: EmblaOptionsType = { loop: true }
+const SLIDE_COUNT = 5
+const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
+    <main className="flex min-h-screen flex-col items-center bg-zinc-600">
+      <div className="flex flex-col min-w-full bg-zinc-800 md:px-80 px-8">
+        <div className="flex flex-row justify-between">
+          <Image
+              src="/websitelogo.png"
+              alt="Logo"
+              className=""
               width={100}
-              height={24}
+              height={100}
               priority
-            />
-          </a>
+          />
+          <div className="flex flex-row flex-wrap content-center">
+            <Link href="" className="">Home</Link>
+            <Link href="" className="pl-8">Creators</Link>
+          </div>
         </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="md:flex md:flex-row md:flex-wrap md:justify-between min-w-full md:px-80 px-8 py-10">
+        <div className="content-center">
+          <h1 className="text-4xl font-bold text-center">Elevating your social presence!</h1>
+        </div>
+        <div className="flex justify-center">
+          <Image
+                src="/websitelogo.png"
+                alt="Logo"
+                className=""
+                width={400}
+                height={400}
+                priority
+          />
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="min-w-full bg-zinc-800 flex flex-col justify-center">
+        <div>
+          <h1 className="text-4xl font-bold text-center py-10">Our Creators</h1>
+        </div>
+        <div className="md:px-80 px-8">
+          <EmblaCarousel slides={SLIDES} options={OPTIONS} />
+        </div>
       </div>
     </main>
   );
---
+++ app/ui/ClientImage.tsx
@@ -0,0 +1,44 @@
+import Image from "next/image"
+
+export default function ClientImage() {
+    return(
+        <div className="group relative rounded-lg">
+            
+            <div className="transition-all ease-in-out duration-500 bg-black/30 opacity-0 absolute min-w-full max-w-full max-h-0 rounded-lg -bottom-20 group-hover:-translate-y-20 group-hover:opacity-100 group-hover:max-h-full backdrop-blur-sm p-4">
+                <h1 className="text-xl text-bold ">MR BEAST</h1>
+                <h1 className="text-lg text-normal pb-4">Gamer</h1>
+                <div className="flex flex-row">
+                    <Image
+                        src="/user.svg"
+                        alt="Subscrubers"
+                        className=""
+                        width={25}
+                        height={25}
+                        priority
+                    />
+                    <h1 className="text-lg text-normal pl-5 pd-2">300,000,000 Subs</h1>
+                </div>
+                <div className="flex flex-row">
+                    <Image
+                        src="/eye.svg"
+                        alt="Viewers"
+                        className=""
+                        width={25}
+                        height={25}
+                        priority
+                    />
+                    <h1 className="text-lg text-normal pl-5">100,000,000 AVG Views</h1>
+                </div>
+
+            </div>
+            <Image
+                src="/placeholder.jpg"
+                alt="Logo"
+                className="rounded-lg"
+                width={300}
+                height={300}
+                priority
+            />
+        </div>
+    );
+}
